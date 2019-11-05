49cf737b45942266345a8a2af2625d918a370d04
kolegran <kolegran@gmail.com>
2019-11-06 00:44:13 +0200

    Added travis.yml for test integration

---
+++ src/test/java/letsfwserver/LetsFwServerTest.java
@@ -0,0 +1,13 @@
+package letsfwserver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LetsFwServerTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
